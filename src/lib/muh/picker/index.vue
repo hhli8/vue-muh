0af96370b318e265f23253aac6964900f7563187
hhli8 <963820266@qq.com>
2020-02-25 18:28:14 +0800

    添加picker

--- src/lib/muh/picker/index.vue
+++ src/lib/muh/picker/index.vue
@@ -1,14 +1,79 @@
 <template>
   <div>
-    <p>picker</p>
+    <button @click="click">dianwo</button>
+    <div class="muh-picker" :style='"height: "+boxHeight+"px;"'>
+      <!--<picker-slot ref="slot" v-for="(item,index) in columns" :list="item.values" :type="item.type" :defaultSelected="item.defaultIndex" :visibleCount="visibleItemCount" @getHeight="setHeight" @getSelect="setSelect" :key="index"></picker-slot>-->
+      <picker-slot ref="slot" v-for="(item,index) in type?columns:[{values: columns, defaultIndex: index}]" :key="index" :list="item.values"
+        :defaultSelected="item.defaultIndex"
+        :visibleCount="visibleItemCount"
+        @getSelect="setSelect"
+        @getHeight="setHeight">
+      </picker-slot>
+    </div>
   </div>
 </template>
 
 <script>
+import pickerSlot from './slot.vue'
 export default {
-  name: 'muhPicker'
+  name: 'muhPicker',
+  components: {
+    pickerSlot
+  },
+  data () {
+    return {
+      boxHeight: ''
+    }
+  },
+  props: {
+    type: {
+      type: String,
+      default: () => {
+        return ''
+      }
+    },
+    columns: {
+      type: Array,
+      default: () => { return [] }
+    },
+    setKey: {
+      type: Object,
+      default: () => { return {} }
+    },
+    visibleItemCount: {
+      type: Number,
+      default: 5
+    },
+  },
+  computed: {
+    value () {
+      return this.setKey.value || 'value'
+    },
+    code () {
+      return this.setKey.code || 'code'
+    },
+    index () {
+      return this.setKey.defaultIndex || 0
+    }
+  },
+  methods: {
+    click () {
+      console.log(this.value, this.code, this.columns, this.visibleItemCount)
+    },
+    setHeight (val) { // 给盒子设置高度
+      this.boxHeight = val
+    },
+    setSelect (val) {
+      //
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped="scoped">
+.muh-picker {
+  display: flex;
+  text-align: center;
+  overflow: hidden;
+}
 </style>
--- src/views/muh/picker/index.vue
+++ src/views/muh/picker/index.vue
@@ -1,14 +1,65 @@
 <template>
   <div>
-    <muh-picker></muh-picker>
+    <p style="text-align: center;padding: 20px 0;font-size: 40px;">this is picker</p>
+    <div style="padding: 0 20px;">
+      <button @click="show1">选择出生年月日(中文版)</button>
+      <button>选择出生年月日(英文版)</button>
+      <button @click="show3">选择单列</button>
+      <button>省市区</button>
+    </div>
+    <muh-popup v-model="dataPicker">
+      <muh-picker type="date"></muh-picker>
+    </muh-popup>
+    <muh-popup v-model="singlePicker">
+      <muh-picker
+        :columns="columns"
+        :set-key="columnsOption"
+        :visibleItemCount='3'>
+      </muh-picker>
+    </muh-popup>
   </div>
 </template>
 
 <script>
 export default {
-  //
+  data () {
+    return {
+      dataPicker: false,
+      singlePicker: false,
+      columns: [],
+      columnsOption: {
+        value: 'name',
+        code: 'code',
+        defaultIndex: 2
+      }
+    }
+  },
+  created () {
+    setTimeout(() => {
+      this.columns = [
+        { name: '杭州', code: '1001' },
+        { name: '宁波', code: '1002' },
+        { name: '金华', code: '1003' },
+        { name: '衢州', code: '1004' },
+        { name: '丽水', code: '1005' },
+        { name: '温州', code: '1006' },
+        { name: '舟山', code: '1007' }
+      ]
+    }, 30)
+  },
+  methods: {
+    show1 () {
+      this.dataPicker = true
+    },
+    show3 () {
+      this.singlePicker = true
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="scss" scoped="scoped">
+button {
+  margin-bottom: 15px;
+}
 </style>
